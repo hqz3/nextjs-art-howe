5e2851e29b7c67a3dbc94ae0f6d42a337052a377
hqz3 <heqzheng@gmail.com>
2023-01-19 16:40:11 -0500

    Update RootLayout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,17 @@
-import './globals.css'
+import "./globals.css";
+import Header from "./Header";
+import SearchBar from "./SearchBar";
+import { EB_Garamond } from "@next/font/google";
+const ebGaramond = EB_Garamond({ subsets: ["latin"], weight: ["400"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html lang="en" className={ebGaramond.className}>
+      <body>
+        <Header />
+        <SearchBar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
