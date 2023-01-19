36bb9914388ae8e458bf8fe71e77f4444f2a850f
hqz3 <heqzheng@gmail.com>
2023-01-19 16:46:34 -0500

    Create Mosaic component

---
+++ app/Mosaic.jsx
@@ -0,0 +1,71 @@
+"use client";
+import { useEffect, useState, useRef } from "react";
+import Image from "next/image";
+
+export default function Mosaic({ mosaicData }) {
+  const mosaicElement = useRef(null);
+  const [shuffled, setShuffled] = useState(false);
+
+  useEffect(() => {
+    const getRandomInt = (max) => {
+      return Math.floor(Math.random() * max);
+    };
+    const swap = (array, idx1, idx2) => {
+      [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+    };
+    // Fisher-Yates algorithm to generate a randomly ordered mosaic
+    for (let i = mosaicData.length - 1; i >= 0; i--) {
+      let randomIdx = getRandomInt(i);
+      swap(mosaicData, i, randomIdx);
+    }
+    setShuffled(true);
+  }, []);
+
+  if (shuffled) {
+    return (
+      <>
+        <div
+          className="mosaic"
+          onMouseOver={handleMosaicHover}
+          ref={mosaicElement}
+        >
+          {mosaicData.map((post) => (
+            <div className="mosaic__container" key={post.postId}>
+              <Image
+                src={post.thumbnail}
+                data-image="pixelated"
+                alt={post.title}
+                height={10}
+                width={10}
+              />
+              <Image
+                src={post.medium}
+                data-image="regular"
+                alt={post.title}
+                height={post.height}
+                width={post.width}
+              />
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+
+  function handleMosaicHover(e) {
+    if (e.target.dataset?.image !== "regular") return;
+
+    const mosaicWidth = mosaicElement.current.offsetWidth;
+    const scrollHeight = document.body.scrollHeight;
+    e.target.classList.remove("hover-right", "hover-left", "up");
+
+    if (e.clientX <= mosaicWidth / 2) {
+      e.target.classList.add("hover-right");
+    } else {
+      e.target.classList.add("hover-left");
+    }
+    if (e.pageY + 150 >= scrollHeight) {
+      // e.target.classList.add("up");
+    }
+  }
+}
