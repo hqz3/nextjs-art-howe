ccb4b1fe8fb442e8e248ab9320bfc18089b5b4d1
hqz3 <heqzheng@gmail.com>
2023-01-20 13:38:41 -0500

    Import react-image-gallery CSS at RootLayout level

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import SearchBar from "./SearchBar";
 import { EB_Garamond } from "@next/font/google";
 const ebGaramond = EB_Garamond({ subsets: ["latin"], weight: ["400"] });
+import "react-image-gallery/styles/css/image-gallery.css";
 
 export default function RootLayout({ children }) {
   return (
