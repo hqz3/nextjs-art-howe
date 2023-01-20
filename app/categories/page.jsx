565527a45cdbe5f6d969aee5a7c6208729b339b0
hqz3 <heqzheng@gmail.com>
2023-01-20 13:20:23 -0500

    Create CategoriesPage

---
+++ app/categories/page.jsx
@@ -0,0 +1,44 @@
+import styles from "./page.module.css";
+
+const sixHours = 1000 * 60 * 60 * 6;
+export const revalidate = sixHours;
+
+async function getAllCategories() {
+  return fetch(process.env.REACT_APP_WORDPRESS_API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          tags(first: 9999999) {
+            nodes {
+              name
+            }
+          }
+        }
+      `,
+    }),
+  })
+    .then((res) => res.json())
+    .then(({ data }) => data.tags.nodes);
+}
+
+export default async function CategoriesPage() {
+  const categories = await getAllCategories();
+
+  return (
+    <div className={styles.container}>
+      {categories.map((tag, idx) => (
+        <div key={idx}>
+          {idx === 0 && <b>{tag.name[0]}</b>}
+          {idx > 0 && tag.name[0] !== categories[idx - 1].name[0] && (
+            <b>{tag.name[0]}</b>
+          )}
+          <div className={styles.tag}>{tag.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
