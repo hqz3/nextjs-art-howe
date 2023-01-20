7c26bf08b2b808ec223bf0339633e2beb37f7f95
hqz3 <heqzheng@gmail.com>
2023-01-19 21:05:52 -0500

    Create ArtworkDetail component

---
+++ app/artwork/[id]/ArtworkDetail.jsx
@@ -0,0 +1,34 @@
+import styles from "./ArtworkDetail.module.css";
+
+export default function ArtworkDetail({ artwork }) {
+  return (
+    <div className={styles.container}>
+      <div id="artist">{artwork.details.artist?.toUpperCase()}</div>
+      <br />
+      <div className="title">{artwork.title}</div>
+      <div id="year">{artwork.details.year}</div>
+      <br />
+      <b>Exhibition</b>
+      <div id="exhibition">{artwork.details.exhibition}</div>
+      <br />
+      <b>Venue</b>
+      <div id="venue">{artwork.details.venue}</div>
+      <br />
+      <b>Date Seen</b>
+      <div>{artwork.details.dateSeen?.replace(/[/]/g, ".")}</div>
+      <br />
+      <b>Tags</b>
+      <div>
+        {artwork.tags.edges.map((tag, idx) => {
+          if (idx === artwork.tags.edges.length - 1) {
+            return <span id="tag" key={idx}>{`${tag.node.name}`}</span>;
+          }
+          return <span id="tag" key={idx}>{`${tag.node.name}; `}</span>;
+        })}
+      </div>
+      <br />
+      <b>Hi-res Link</b>
+      <a href={artwork.details.link}>{artwork.details.link}</a>
+    </div>
+  );
+}
