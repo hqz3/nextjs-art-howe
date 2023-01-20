c0a89f3480930800cddd7f0d2346cd1e55fcb63d
hqz3 <heqzheng@gmail.com>
2023-01-20 17:10:38 -0500

    Wrap tags with Link

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -1,28 +1,9 @@
+import Link from "next/link";
 import styles from "./page.module.css";
+import getAllCategories from "@/queries/getAllCategories";
+import convertToSlug from "@/utils/convertToSlug";
 
 export const revalidate = process.env.REVALIDATE_TIME;
-
-async function getAllCategories() {
-  return fetch(process.env.REACT_APP_WORDPRESS_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: `
-        query {
-          tags(first: 9999999) {
-            nodes {
-              name
-            }
-          }
-        }
-      `,
-    }),
-  })
-    .then((res) => res.json())
-    .then(({ data }) => data.tags.nodes);
-}
 
 export default async function CategoriesPage() {
   const categories = await getAllCategories();
@@ -35,7 +16,12 @@ export default async function CategoriesPage() {
           {idx > 0 && tag.name[0] !== categories[idx - 1].name[0] && (
             <b>{tag.name[0]}</b>
           )}
-          <div className={styles.tag}>{tag.name}</div>
+          <Link
+            href={"/categories/" + convertToSlug(tag.name)}
+            className={styles.tag}
+          >
+            {tag.name}
+          </Link>
         </div>
       ))}
     </div>
