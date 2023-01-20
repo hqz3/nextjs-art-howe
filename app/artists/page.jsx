570a862c0b824619168010aaaf4b8dd73d946e15
hqz3 <heqzheng@gmail.com>
2023-01-20 13:20:04 -0500

    Change code layout

--- app/artists/page.jsx
+++ app/artists/page.jsx
@@ -1,29 +1,7 @@
 import styles from "./page.module.css";
 
-export default async function ArtistsPage() {
-  const artists = await getAllArtists();
-  // Remove duplicate artists
-  const set = new Set();
-  artists.forEach((artist) => {
-    set.add(artist.details.artist);
-  });
-  // Sort the artists in alphabetical order
-  const filteredArtistsData = [...set].sort();
-
-  return (
-    <div className={styles.container}>
-      {filteredArtistsData.map((artist, idx) => (
-        <div key={idx}>
-          {idx === 0 && <b>{artist[0]}</b>}
-          {idx > 0 && artist[0] !== filteredArtistsData[idx - 1][0] && (
-            <b>{artist[0]}</b>
-          )}
-          <div className={styles.artist}>{artist}</div>
-        </div>
-      ))}
-    </div>
-  );
-}
+const sixHours = 1000 * 60 * 60 * 6;
+export const revalidate = sixHours;
 
 async function getAllArtists() {
   return fetch(process.env.REACT_APP_WORDPRESS_API_URL, {
@@ -47,4 +25,29 @@ async function getAllArtists() {
   })
     .then((res) => res.json())
     .then(({ data }) => data.posts.nodes);
+}
+
+export default async function ArtistsPage() {
+  const artists = await getAllArtists();
+  // Remove duplicate artists
+  const set = new Set();
+  artists.forEach((artist) => {
+    set.add(artist.details.artist);
+  });
+  // Sort the artists in alphabetical order
+  const filteredArtistsData = [...set].sort();
+
+  return (
+    <div className={styles.container}>
+      {filteredArtistsData.map((artist, idx) => (
+        <div key={idx}>
+          {idx === 0 && <b>{artist[0]}</b>}
+          {idx > 0 && artist[0] !== filteredArtistsData[idx - 1][0] && (
+            <b>{artist[0]}</b>
+          )}
+          <div className={styles.artist}>{artist}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
