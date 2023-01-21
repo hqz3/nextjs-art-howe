df004bd1bcc3f930fa05c34fdb010d6c70467f23
hqz3 <heqzheng@gmail.com>
2023-01-20 19:44:57 -0500

    Complete adding Links to artwork details

--- app/artwork/[id]/ArtworkDetail.jsx
+++ app/artwork/[id]/ArtworkDetail.jsx
@@ -1,18 +1,27 @@
 import styles from "./ArtworkDetail.module.css";
+import Link from "next/link";
 
 export default function ArtworkDetail({ artwork }) {
   return (
     <div className={styles.container}>
-      <div id="artist">{artwork.details.artist.toUpperCase()}</div>
+      <Link href={"/artists/" + artwork.details.artist} id="artist">
+        {artwork.details.artist.toUpperCase()}
+      </Link>
       <br />
       <div className="title">{artwork.title}</div>
-      <div id="year">{artwork.details.year}</div>
+      <Link href={"/search/" + artwork.details.year} id="year">
+        {artwork.details.year}
+      </Link>
       <br />
       <b>Exhibition</b>
-      <div id="exhibition">{artwork.details.exhibition}</div>
+      <Link href={"/search/" + artwork.details.exhibition} id="exhibition">
+        {artwork.details.exhibition}
+      </Link>
       <br />
       <b>Venue</b>
-      <div id="venue">{artwork.details.venue}</div>
+      <Link href={"/search/" + artwork.details.venue} id="venue">
+        {artwork.details.venue}
+      </Link>
       <br />
       <b>Date Seen</b>
       <div>{artwork.details.dateSeen.replace(/[/]/g, ".")}</div>
@@ -21,14 +30,32 @@ export default function ArtworkDetail({ artwork }) {
       <div>
         {artwork.tags.edges.map((tag, idx) => {
           if (idx === artwork.tags.edges.length - 1) {
-            return <span id="tag" key={idx}>{`${tag.node.name}`}</span>;
+            return (
+              <Link
+                href={"/search/" + tag.node.name}
+                id="tag"
+                key={idx}
+              >{`${tag.node.name}`}</Link>
+            );
           }
-          return <span id="tag" key={idx}>{`${tag.node.name}; `}</span>;
+          return (
+            <Link
+              href={"/search/" + tag.node.name}
+              id="tag"
+              key={idx}
+            >{`${tag.node.name}; `}</Link>
+          );
         })}
       </div>
       <br />
       <b>Hi-res Link</b>
-      <a href={artwork.details.link}>{artwork.details.link}</a>
+      <a
+        id="sourceLink"
+        className={styles.sourceLink}
+        href={artwork.details.link}
+      >
+        {artwork.details.link}
+      </a>
     </div>
   );
 }
