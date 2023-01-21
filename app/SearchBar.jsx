f0b2de64faaf6aabf537a95576e67ea79d36e75c
hqz3 <heqzheng@gmail.com>
2023-01-20 19:22:36 -0500

    Handle search queries

--- app/SearchBar.jsx
+++ app/SearchBar.jsx
@@ -1,13 +1,17 @@
 "use client";
 import { useRef } from "react";
 import styles from "./SearchBar.module.css";
+import { useRouter } from "next/navigation";
 
 export default function SearchBar() {
   const searchBarElement = useRef(null);
+  const router = useRouter();
 
   function handleSearch(e) {
     e.preventDefault();
+    const query = searchBarElement.current.value;
     searchBarElement.current.value = "";
+    router.push("/search/" + query);
   }
 
   return (
