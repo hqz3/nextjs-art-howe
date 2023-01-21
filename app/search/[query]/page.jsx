e595060e6e4b13f92fd14645a8e2330c7998d77c
hqz3 <heqzheng@gmail.com>
2023-01-20 19:08:00 -0500

    Create SearchPage

---
+++ app/search/[query]/page.jsx
@@ -0,0 +1,17 @@
+import styles from "./page.module.css";
+import Mosaic from "@/app/Mosaic";
+import getSearchTerm from "@/queries/getSearchTerm";
+import generateMosaicData from "@/utils/generateMosaicData";
+
+export default async function SearchPage({ params: { query } }) {
+  const decodedQuery = decodeURIComponent(query);
+
+  const mosaicData = await generateMosaicData(
+    getSearchTerm(decodedQuery, "THUMBNAIL"),
+    getSearchTerm(decodedQuery, "MEDIUM")
+  );
+
+  if (mosaicData.length) {
+    return <Mosaic mosaicData={mosaicData} randomize={false} />;
+  } else return <div className={styles.notFound}>No results found</div>;
+}
