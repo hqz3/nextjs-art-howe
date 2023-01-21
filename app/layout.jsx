bca1d8ee4adf4b1831de915c5113962260da4427
hqz3 <heqzheng@gmail.com>
2023-01-21 17:48:46 -0500

    Update font import

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,11 @@
 import "./globals.css";
+import "react-image-gallery/styles/css/image-gallery.css";
 import Header from "./Header";
 import SearchBar from "./SearchBar";
-import { EB_Garamond } from "@next/font/google";
-const ebGaramond = EB_Garamond({ subsets: ["latin"], weight: ["400"] });
-import "react-image-gallery/styles/css/image-gallery.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={ebGaramond.className}>
+    <html lang="en">
       <body>
         <Header />
         <SearchBar />
