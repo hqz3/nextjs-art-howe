f64420b5be2682155dc3856688866567985931de
hqz3 <heqzheng@gmail.com>
2023-01-23 04:58:36 -0500

    Fix mobile responsiveness bug

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,10 @@ import SearchBar from "./SearchBar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+      />
       <body>
         <Nav />
         <SearchBar />
